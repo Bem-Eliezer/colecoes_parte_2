b8430374a16c3a963f8a2310344a7a933be00d95
Bem-Eliezer <eliezer.santos1010@gmail.com>
2024-12-12 19:39:58 -0300

    correção do projeto

---
+++ ValorMedia.java
@@ -0,0 +1,18 @@
+public class ValorMedia {
+
+
+   public static void main(String args[]) {
+      int notaDoJoao = 10;
+      int notaDoPedro = 6;
+      int notaDaAna = 4;
+      int notaDaMaria = 7;
+
+    
+    int mediaDaNota = (notaDoJoao + notaDoPedro + notaDaAna + notaDaMaria)/4;
+    
+    System.out.println("A media é: " + mediaDaNota);
+
+   }
+
+}
+
